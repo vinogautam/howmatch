747cc8944edfca6f0b362ff9e09f9cfa5a7ec550
vinogautam <dhanavel237vino@gmail.com>
2020-05-17 14:23:13 +0530

    User Api

--- admin/api/common.php
+++ admin/api/common.php
@@ -71,3 +71,51 @@ function get_count($sql){
 function get_data_by_id($table, $id){
 	return get_row("select * from $table where id = $id");
 }
+
+function get_all_meta($table, $id){
+	$res = get_results("select * from meta_data where ref = '$table' and ref_id = $id");
+	$new_res = array();
+
+	foreach ($res as $key => $value) {
+		$data = @unserialize($value['meta_value']);
+		if ($data !== false) {
+		    $new_res[$value['meta_name']] = $data;
+		} else {
+		    $new_res[$value['meta_name']] = $value['meta_value'];
+		}
+	}
+	return $new_res;
+}
+
+function get_meta($table, $id, $name){
+	$res = get_row("select * from meta_data where ref = '$table' and meta_name = '$name' and ref_id = $id");
+	$value = isset($res['meta_value']) ? $res['meta_value'] : '';
+	$data = @unserialize($value);
+	if ($data !== false) {
+	    return $data;
+	} else {
+	    return $value;
+	}
+}
+
+function set_meta($table, $id, $name, $value){
+	$res = get_row("select * from meta_data where ref = '$table' and meta_name = '$name' and ref_id = $id");
+
+	$value = is_array($value) ? serialize($value) : $value;
+
+	if(isset($res['id'])){
+		update('meta_data', array('meta_value' => $value), array('id' => $res['id']));
+	} else {
+		insert('meta_data', array('ref' => $table, 'ref_id' => $id, 'meta_name' => $name, 'meta_value' => $value));
+	}
+}
+
+function hm_test_meta(){
+	echo '<pre>';
+	echo get_meta('users', 1, 'first_name');
+	set_meta('users', 1, 'roles', array('a', 'b'));
+	print_r(get_meta('users', 1, 'roles'));
+	set_meta('users', 1, 'roles', array('b', 'c'));
+	print_r(get_meta('users', 1, 'roles'));
+	print_r(get_all_meta('users', 1));
+}
--- admin/api/users.php
+++ admin/api/users.php
@@ -14,12 +14,73 @@ function hm_login(){
 	}
 }
 
-function hm_candidates(){
+function hm_users(){
 	$res = get_row("select * from users where user_type = 2");
+	$new_res = array();
+	foreach ($res as $key => $value) {
+		$value['profile'] = get_all_meta('users', $value['id']);
+		unset($value['password']);
+	}
 	return array('status' => 'Success', 'data' => $res);
 }
 
 function hm_company(){
-	$res = get_row("select * from users where user_type = 3");
+	$res = get_row("select * from users where user_type = 2");
+	$new_res = array();
+	foreach ($res as $key => $value) {
+		$value['profile'] = get_all_meta('users', $value['id']);
+		unset($value['password']);
+	}
 	return array('status' => 'Success', 'data' => $res);
+}
+
+function hm_save_user(){
+	$profile = $_POST['profile'];
+	$data = array(
+		'username' => $_POST['username'],
+		'email' => $_POST['email'],
+		'user_type' => $_POST['user_type'],
+		'updated_on' => date('Y-m-d H:i:s')
+	);
+	if(isset($_POST['id'])){
+		if(isset($_POST['password'])){
+			$data['password'] = md5($_POST['password']);
+		}
+		$id = $_POST['id'];
+		update('users', $data, array('id' => $id));
+		$res = array('status' => 'Success', 'msg' => 'Users Updated Successfully');
+	} else {
+		$data['password'] = md5($_POST['password']);
+		$data['registered_on'] = date('Y-m-d H:i:s');
+		$data['status'] = 1;
+		insert('users', $data);
+		$id = last_id();
+		$res = array('status' => 'Success', 'msg' => 'Users Added Successfully');
+	}
+
+	foreach ($profile as $key => $value) {
+		set_meta('users', $id, $key, $value);
+	}
+
+	return $res;
+}
+
+function hm_delete_user(){
+	$data = $_POST['delete'];
+
+	foreach ($data as $key => $value) {
+		delete('users', array('id' => $value));
+	}
+
+	return array('status' => 'Success', 'msg' => 'Users Deleted Successfully');
+}
+
+function hm_change_user_status(){
+	update('users', array('status' => $_POST['status']), array('id' => $_POST['id']));
+
+	if($_POST['status']){
+		return array('status' => 'Success', 'msg' => 'Users Published Successfully');
+	} else {
+		return array('status' => 'Success', 'msg' => 'Users Unpublished Successfully');
+	}
 }
