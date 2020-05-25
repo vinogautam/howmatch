fb98231dc26faeaad1c9ff201bbcd4db6465c93f
harinath12 <harinathit@gmail.com>
2020-05-25 09:38:28 +0530

    html

---
+++ html/js/custom.js
@@ -0,0 +1,29 @@
+// $('#myCarousel').carousel({
+//   interval: 10000
+// })
+
+// $('.carousel .carousel-item').each(function(){
+//     var minPerSlide = 3;
+//     var next = $(this).next();
+//     if (!next.length) {
+//     next = $(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
+    
+//     for (var i=0;i<minPerSlide;i++) {
+//         next=next.next();
+//         if (!next.length) {
+//         	next = $(this).siblings(':first');
+//       	}
+        
+//         next.children(':first-child').clone().appendTo($(this));
+//       }
+// });
+
+$('.panel-collapse').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-heading').addClass('active');
+});
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-heading').removeClass('active');
+});
