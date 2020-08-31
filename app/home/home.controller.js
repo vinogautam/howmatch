18d91ad358b5022b44f3ef2b655ffb399b339ba9
harinath12 <harinathit@gmail.com>
2020-08-31 16:56:43 +0530

    corosuel update

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -28,4 +28,27 @@ function homeController($rootScope, $scope, $state, ApiService, $window, $timeou
         $rootScope.search = angular.copy($scope.pageInfo.search);
         $state.go('job_search');
     };
+
+    $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
 }
