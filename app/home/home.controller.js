7b1a6186c94f7e18967510b8c0222599862a367f
vinogautam <dhanavel237vino@gmail.com>
2020-09-02 09:14:39 +0530

    update

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -84,27 +84,28 @@ function homeController($rootScope, $scope, $state, ApiService, $window, $timeou
                 });
             });
         });
+
+        $timeout(function(){
+            $('.customer-logos').slick({
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 1500,
+                arrows: false,
+                dots: false,
+                pauseOnHover: false,
+                responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                }, {
+                    breakpoint: 520,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }]
+            });
+        }, 1000);
     });
-    $(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
 }
