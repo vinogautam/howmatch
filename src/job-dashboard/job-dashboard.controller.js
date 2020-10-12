ae180dc4c225a2863f4bbf86c6349ca527636fc0
vinogautam <dhanavel237vino@gmail.com>
2020-10-12 15:42:48 +0530

    Drag and Drop

--- src/job-dashboard/job-dashboard.controller.js
+++ src/job-dashboard/job-dashboard.controller.js
@@ -5,6 +5,26 @@ jobdashboardController.$inject = [ '$stateParams', '$rootScope', '$scope', '$sta
 function jobdashboardController($stateParams, $rootScope, $scope, $state, ApiService, $window, $timeout, $interval) {
 	$scope.pageInfo = {};
 
-	
+	$( ".user-short-info").draggable({
+      cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+      revert: "invalid", // when not dropped, the item will revert back to its initial position
+      containment: "document",
+      helper: "clone",
+      cursor: "move",
+      classes: {
+	    "ui-draggable": "highlight"
+	  }
+    });
+ 
+    // Let the trash be droppable, accepting the gallery items
+    $('.draggable-panel').droppable({
+      accept: ".user-short-info",
+      classes: {
+        "ui-droppable-active": "ui-state-highlight"
+      },
+      drop: function( event, ui ) {
+        //deleteImage( ui.draggable );
+      }
+    });
 
 }
